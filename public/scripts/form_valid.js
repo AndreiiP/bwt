06521b00dd94ea203a5ddcf82d04d1fd7b0fac46
AndreiiP <sonata7x@ya.ru>
2018-04-11 12:02:31 +0300

    add js valid

---
+++ public/scripts/form_valid.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+		// name
+		$("#name").focus(function() {
+			$("#name").keyup(function(){
+			if($('#name').val() == ''){
+				$('#name').css('border', 'solid 1px red');
+				$('#name').attr("placeholder", "enter name here");
+			}else{
+				$('#name').css('border', 'solid 1px #e5e5e5');
+			}
+		});
+	});
+		//surname
+		$("#surname").focus(function() {
+			$("#surname").keyup(function(){
+			if($('#surname').val() == ''){
+				$('#surname').css('border', 'solid 1px red');
+				$('#surname').attr("placeholder", "enter surname here");
+			}else{
+				$('#surname').css('border', 'solid 1px #e5e5e5');
+			}
+		  });
+		});
+		
+		// //email
+		$("#email").focus(function() {
+			$("#email").keyup(function(){
+					var email = $(this).val();
+					if (email.length > 0
+					&& (email.match(/.+?\@.+/g) || []).length !== 1) {
+						$('#email').css('border', 'solid 1px red');
+						$('#email').attr("placeholder", "enter correct email here");
+					} else {
+						$('#email').css('border', 'solid 1px #e5e5e5');
+					}
+		});
+	});
+
+
+});
