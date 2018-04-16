2bbdcad0957542792c37c8de5ad64f5a7eb62c58
Andrii <andreii_usa@yahoo.com>
2018-04-17 01:36:22 +0300

    remove head

--- application/views/feedbacklist/index.php
+++ application/views/feedbacklist/index.php
@@ -1,27 +1,4 @@
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-<link href="/public/css/style.css" type="text/css" rel="stylesheet">
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-
-<div class="content feedlist">
-    <nav class="navbar-inverse">
-        <div class="collapse navbar-collapse" id="main-menu" >
-            <ul class="nav navbar-nav navbar-left nav-color">
-                <li><a href="/" class="nav-color">Home</a></li>
-                <li><a href="/weather" class="nav-color">Weather</a></li>
-                <li><a href="/feedback" class="nav-color">Feedback</a></li>
-                <li><a href="/feedbacklist" class="nav-color">feedbacklist</a></li>
-                <li><a href="#" class="nav-color user"><?php if(isset($_SESSION['user'])){echo "Пользователь: ".$_SESSION['user']; }?></a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <form class="form-horizontal" method="post" id="">
-                    <?php if(isset($_SESSION['user'])){ ?> <input type="submit" value="LogOut" name="Out" class="btn btn-primary LogOut"> <?php } ?>
-                </form>
-            </ul>
-
-        </div>
-    </nav>
-</div>
+<div class="w_z">
 <?php if(isset($_SESSION['user'])){ ?>
     <?php foreach ($feed as $value){?>
         <div class="col-md-2"></div>
@@ -64,6 +41,7 @@
         </div>
     </form>
 <?php } ?>
+</div>
 <?php if(isset($_POST['Out']) == "LogOut"){
     unset($_SESSION["user"]);
     session_destroy();
